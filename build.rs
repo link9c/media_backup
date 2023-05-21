647d5abfce50569f4c4f700cf669c8c03d8ad181
Your Name <you@example.com>
2023-05-21 16:30:08 +0800

    v0.1

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@
 fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("ui/images/icon.jpg");
+        res.set_icon("ui/images/icon.ico");
         res.compile().unwrap();
       }
     slint_build::compile("ui/appwindow.slint").unwrap();
